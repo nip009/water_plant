9c7a6bcc04dd0141664d3b2db9568f029eace213
Simen Lien <simenlien1@hotmail.com>
2020-04-12 15:48:03 +0200

    Add dropdown in notifications settings

--- lib/src/screens/settings/components/notifications.dart
+++ lib/src/screens/settings/components/notifications.dart
@@ -18,6 +18,8 @@ class _NotificationSettingsState extends State<NotificationSettings> {
   bool _deviceWifiDisconnect = true;
   bool _lowWater = true;
   bool _lowBattery = true;
+
+  String _selectedTime;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,144 +34,203 @@ class _NotificationSettingsState extends State<NotificationSettings> {
         ),
         centerTitle: true,
       ),
-      body: Container(
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              PageTitle('Notifications'),
-              Row(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            PageTitle('Notifications'),
+            Row(
+              children: <Widget>[
+                formTitle('Push notifications'),
+                Container(
+                  child: Icon(
+                    Icons.notifications,
+                    color: Colors.grey[700],
+                    size: 18,
+                  ),
+                ),
+              ],
+            ),
+            FormCard(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  formTitle('Push notifications'),
-                  Container(
-                    child: Icon(
-                      Icons.notifications,
-                      color: Colors.grey[700],
-                      size: 18,
+                  Text('Turn on push notifications'),
+                  Switch(
+                      value: _pushNotificationsOn,
+                      onChanged: (bool newValue) {
+                        setState(() {
+                          _pushNotificationsOn = newValue;
+                        });
+                      }),
+                ],
+              ),
+            ),
+            formTitle('Reminder'),
+            FormCard(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text('Every'),
+                  ),
+                  //TODO: Let the user choose from a list how often (30 min, every hour, every second hour etc.)
+                  Expanded(
+                    child: Container(),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Card(
+                      elevation: 0,
+                      margin: EdgeInsets.all(0),
+                      child: Container(
+                        padding: EdgeInsets.only(
+                          left: 12,
+                          right: 5,
+                          top: 5,
+                          bottom: 5,
+                        ),
+                        alignment: Alignment.centerLeft,
+                        color: Colors.white,
+                        child: Container(
+                          padding: EdgeInsets.only(left: 90, right: 10),
+                          /*decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            border: Border.all(
+                              width: 1,
+                              color: Colors.grey[700],
+                            ),
+                          ),*/
+                          child: DropdownButton<String>(
+                            value: _selectedTime,
+                            items: [
+                              '15 min',
+                              '30 min',
+                              '1 hour',
+                              '2 hours',
+                              '3 hours',
+                              '6 hours',
+                              '9 hours',
+                              '12 hours',
+                              '24 hours',
+                            ].map((String value) {
+                              return new DropdownMenuItem<String>(
+                                value: value,
+                                child: Container(
+                                  //alignment: Alignment.centerRight,
+                                  child: new Text(value),
+                                ),
+                              );
+                            }).toList(),
+                            onChanged: (String value) {
+                              setState(() {
+                                _selectedTime = value;
+                              });
+                            },
+                            isExpanded: true,
+                            underline: Container(),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ],
               ),
-              FormCard(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text('Turn on push notifications'),
-                    Switch(
-                        value: _pushNotificationsOn,
-                        onChanged: (bool newValue) {
-                          setState(() {
-                            _pushNotificationsOn = newValue;
-                          });
-                        }),
-                  ],
-                ),
+            ),
+            formTitle('Plants'),
+            FormCard(
+              height: 96,
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text('Critical moisture level'),
+                      Switch(
+                          value: _notifyCriticalMoistureLevel,
+                          onChanged: (bool newValue) {
+                            setState(() {
+                              _notifyCriticalMoistureLevel = newValue;
+                            });
+                          }),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text('Low moisture level'),
+                      Switch(
+                          value: _notifyLowMoistureLevel,
+                          onChanged: (bool newValue) {
+                            setState(() {
+                              _notifyLowMoistureLevel = newValue;
+                            });
+                          }),
+                    ],
+                  ),
+                ],
               ),
-              formTitle('Reminder'),
-              FormCard(
-                child: Row(
-                  children: <Widget>[
-                    Text('Every'),
-                    //TODO: Let the user choose from a list how often (30 min, every hour, every second hour etc.)
-                  ],
-                ),
+            ),
+            formTitle('Device'),
+            FormCard(
+              height: 192,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text('Automatic watering'),
+                      Switch(
+                          value: _automaticWatering,
+                          onChanged: (bool newValue) {
+                            setState(() {
+                              _automaticWatering = newValue;
+                            });
+                          }),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text('Device WIFI disconnect'),
+                      Switch(
+                          value: _deviceWifiDisconnect,
+                          onChanged: (bool newValue) {
+                            setState(() {
+                              _deviceWifiDisconnect = newValue;
+                            });
+                          }),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text('Low water'),
+                      Switch(
+                          value: _lowWater,
+                          onChanged: (bool newValue) {
+                            setState(() {
+                              _lowWater = newValue;
+                            });
+                          }),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text('Low battery'),
+                      Switch(
+                          value: _lowBattery,
+                          onChanged: (bool newValue) {
+                            setState(() {
+                              _lowBattery = newValue;
+                            });
+                          }),
+                    ],
+                  ),
+                ],
               ),
-              formTitle('Plants'),
-              FormCard(
-                height: 96,
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text('Critical moisture level'),
-                        Switch(
-                            value: _notifyCriticalMoistureLevel,
-                            onChanged: (bool newValue) {
-                              setState(() {
-                                _notifyCriticalMoistureLevel = newValue;
-                              });
-                            }),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text('Low moisture level'),
-                        Switch(
-                            value: _notifyLowMoistureLevel,
-                            onChanged: (bool newValue) {
-                              setState(() {
-                                _notifyLowMoistureLevel = newValue;
-                              });
-                            }),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              formTitle('Device'),
-              FormCard(
-                height: 192,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text('Automatic watering'),
-                        Switch(
-                            value: _automaticWatering,
-                            onChanged: (bool newValue) {
-                              setState(() {
-                                _automaticWatering = newValue;
-                              });
-                            }),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text('Device WIFI disconnect'),
-                        Switch(
-                            value: _deviceWifiDisconnect,
-                            onChanged: (bool newValue) {
-                              setState(() {
-                                _deviceWifiDisconnect = newValue;
-                              });
-                            }),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text('Low water'),
-                        Switch(
-                            value: _lowWater,
-                            onChanged: (bool newValue) {
-                              setState(() {
-                                _lowWater = newValue;
-                              });
-                            }),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text('Low battery'),
-                        Switch(
-                            value: _lowBattery,
-                            onChanged: (bool newValue) {
-                              setState(() {
-                                _lowBattery = newValue;
-                              });
-                            }),
-                      ],
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
